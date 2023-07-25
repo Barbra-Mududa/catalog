3c162e72c55d7691b5ab20ec98182a2fdd2f5c47
Barbra Mududa <mududabarbs2017@gmail.com>
2023-07-25 10:11:09 +0300

    feat: app structure and routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Catalog from './pages/Catalog';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className='w-screen h-screen flex flex-row flex-grow'>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/catalog" element={<Catalog />} />
+      </Routes>
+  </div>
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,205 @@
+// //  icons
+// import {
+//     FiGithub,
+//     FiLinkedin,
+//     FiMail,
+//     FiFigma
+//   } from 'react-icons/fi';
+  
+//   // projects imagespng
+//   import Project1 from './assets/img/projects/first-portfolio.png';
+//   import Project2 from './assets/img/projects/safiri.png';
+//   import Project3 from './assets/img/projects/image3.jpg';
+//   import Project4 from './assets/img/projects/vikali.png';
+//   import Project5 from './assets/img/projects/movieforum.png';
+//   import Project6 from './assets/img/projects/sw-eet-living.png';
+  
+//   // skills images
+//   import SkillImg1 from './assets/img/skills/html5.png';
+//   import SkillImg2 from './assets/img/skills/css3.png';
+//   import SkillImg3 from './assets/img/skills/js.png';
+//   import SkillImg4 from './assets/img/skills/reactjs.png';
+//   import SkillImg5 from './assets/img/skills/ruby.png';
+//   // import SkillImg6 from './assets/img/skills/rails.png';
+//   import SkillImg7 from './assets/img/skills/mysql.png';
+//   import SkillImg8 from './assets/img/skills/nodejs.png';
+//   import SkillImg9 from './assets/img/skills/git.png';
+//   import SkillImg10 from './assets/img/skills/figma.png';
+//   import SkillImg11 from './assets/img/skills/jira.png';
+  
+  
+//   // blogs images
+//   import Image1 from './assets/img/blog/app-service.png';
+//   import Image2 from './assets/img/blog/sql.png';
+//   import Image3 from './assets/img/blog/static.png';
+//   import Image4 from './assets/img/blog/disks.png';
+//   import Image5 from './assets/img/blog/ec2.png';
+  
+//   // navigation
+//   export const navigation = [
+//     {
+//       name: 'home',
+//       href: 'home',
+//     },
+//     {
+//       name: 'about',
+//       href: 'about',
+//     },
+//     {
+//       name: 'projects',
+//       href: 'projects',
+//     },
+//     {
+//       name: 'blogs',
+//       href: 'blogs',
+//     },
+//     {
+//       name: 'contact',
+//       href: 'contact',
+//     },
+//   ];
+  
+//   // social
+//   export const social = [
+//     {
+//       icon: <FiMail />,
+//       href: '/contact',
+//     },
+//     {
+//       icon: <FiFigma />,
+//       href: 'https://www.figma.com/files/drafts?fuid=1202324557437039500',
+//     },
+//     {
+//       icon: <FiGithub />,
+//       href: 'https://github.com/Barbra-Mududa',
+//     },
+//     {
+//       icon: <FiLinkedin/>,
+//       href: 'https://www.linkedin.com/in/barbra-mududa-30a36124b/',
+//     },
+//   ];
+  
+//   // projects
+//   export const project = [
+//     {
+//       projectImg: Project1,
+//       projectName: 'About-Me',
+//       projectText: 'From impactful projects to successful collaborations, this showcase embodies the culmination of my skills and serves as a testament to my commitment to excellence in every endeavor.',
+//       demoLink: '',
+//       githubLink: ''
+//     },
+//     {
+//       projectImg: Project2,
+//       projectName: 'Safiri-Kenya',
+//       projectText: 'Embark on unforgettable adventures through exhilarating  trails, immersive camping experiences, and breathtaking Kenya Safaris that will ignite your spirit.',
+//       demoLink: '',
+//       githubLink: ''
+//     },
+//     {
+//       image: Project3,
+//       projectName: 'Enigmatic',
+//       projectText: 'Hiking events website that enables hikers explore the beauty of nature in Kenya. The hiker is able to pick a hike of choice and join the rest of the hiking community in its adventures.',
+//       demoLink: '',
+//       githubLink: ''
+//     },
+//     {
+//       projectImg: Project4,
+//       projectName: 'Vikali',
+//       projectText: 'Vikali seeks to create an online store for consumers so that it is simple to shop for your favorite items from the vast array of online shopping websites that are readily available on the internet.',
+//       demoLink: '',
+//       githubLink: ''
+//     },
+//     {
+//       projectImg: Project5,
+//       projectName: 'Movie-Forum',
+//       projectText: 'This is a simple web app that allows movie lovers around the world to come to our platform and checkout the latest and highest rated films.A user is able to view the latest titles and reviews by other users.',
+//       demoLink: '',
+//       githubLink: ''
+//     },
+//     {
+//       projectImg: Project6,
+//       projectName: 'Sweet-Living',
+//       projectText: 'Sweet-Living website allows realtors and users around the world to buy,sell and rent real estate properties.A user is able to view the available real estate properties and contact the agent involved for purchase.',
+//       demoLink: '',
+//       githubLink: ''
+//     },
+//   ];
+  
+  
+  
+//   // skill
+//   export const skills = [
+//     {
+//       image: SkillImg1,
+//     },
+//     {
+//       image: SkillImg2,
+//     },
+//     {
+//       image: SkillImg3,
+//     },
+//     {
+//       image: SkillImg4,
+//     },
+//     {
+//       image: SkillImg5,
+//     },
+//     // {
+//     //   image: SkillImg6,
+//     // },
+//     {
+//       image: SkillImg7,
+//     },
+//     {
+//       image: SkillImg8,
+//     },
+//     {
+//       image: SkillImg9,
+//     },
+//     {
+//       image: SkillImg10,
+//     },
+//     {
+//       image: SkillImg11,
+//     },
+//   ];
+  
+//   // blogs
+//   export const blogs = [
+//     {
+//       blogImg: Image1,
+//       blogName:'Azure App Service with a SQL Database using Node.js',
+//       blogText:
+//         'Discover the synergy of app service and SQL database: efficient application development and seamless data management for optimal performance.',
+//       blogLink: 'https://dev.to/b_mududa/creating-an-azure-app-service-with-a-sql-database-using-nodejs-lj3',
+//     },
+//     {
+//       blogImg: Image2,
+//       blogName:'SQL Commands Guide',
+//       blogText:
+//         'SQL, or Structured Query Language, is a programming language used to manage data stored in relational databases.SQL statements are used to manage and manipulate data in these tables...',
+//       blogLink: 'https://dev.to/b_mududa/sql-statements-guidecheat-sheet-4em4',
+//     },
+//     {
+//       blogImg: Image3,
+//       blogName:'Creating a Static Web App on Azure Using a Linux VM',
+//       blogText:
+//         'Static web apps have grown in popularity for a wide range of applications. You can quickly deploy your React project as a static web app on Microsoft Azure ...',
+//       blogLink: 'https://dev.to/b_mududa/creating-a-static-web-app-on-azure-using-a-linux-vm-deploy-your-react-app-with-create-static-web-app-m7l',
+//     },
+//     {
+//       blogImg: Image4,
+//       blogName:'Guide to adding storage disks to Azure Linux VM',
+//       blogText:
+//         'Azure Disks create more storage or flexibility.This step-by-step guide will show you how to create, connect, and format a data disk for your Linux VM in Azure...',
+//       blogLink: 'Olivia Doe',
+//     },
+//     {
+//       blogImg: Image5,
+//       blogName:'Mastering the Art of Instance Management',
+//       blogText:
+//         'Amazon Elastic Compute Cloud (EC2) is a cloud computing service that provides scalable computing capacity in the Amazon Web Services (AWS) cloud...',
+//       blogLink: 'https://dev.to/b_mududa/mastering-the-art-of-instance-management-a-comprehensive-guide-to-creating-and-terminating-instances-like-a-pro-2kio',
+//     },
+//   ];
+  
