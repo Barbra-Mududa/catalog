eb27c1938b48efd1b5875a88ab3b7b41a18d6ece
Barbra Mududa <mududabarbs2017@gmail.com>
2023-07-26 16:06:17 +0300

    fix: fix screen width conflicts

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from './pages/Home';
 
 function App() {
   return (
-    <div className='w-screen h-screen flex flex-row flex-grow'>
+    <div className='h-screen'>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/catalog" element={<Catalog />} />
