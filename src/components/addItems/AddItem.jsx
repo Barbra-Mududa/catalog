99392d81b4397402e439d8a7c2494c055ab496c4
Barbra Mududa <mududabarbs2017@gmail.com>
2023-07-26 16:06:53 +0300

    feat: header component

---
+++ src/components/addItems/AddItem.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const AddItem = () => {
+
+  return (
+    <div>
+        <div>
+            <h2>Add Item</h2>
+        </div>
+    </div>
+  )
+}
+
+export default AddItem;
