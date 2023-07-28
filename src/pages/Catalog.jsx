5686c401e134cd0c72d5f04e4bedb6a06227cd99
Barbra Mududa <mududabarbs2017@gmail.com>
2023-07-28 13:00:50 +0300

    refactor: change catalog page styling

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -8,13 +8,13 @@ const Catalog = () => {
     <Layout>
       <div className="flex flex-col flex-grow justify-center items-center h-full"> {/* Adjust styles here */}
         <div className='font-lato'>
-          <h2 className='text-xl font-semibold text-center'>You have no recorde to show</h2>
-          <p className='text-base font-normal text-center'>Add items to your catalog to get started</p>
+          <h2 className='text-base font-semibold text-center'>You have no recorde to show</h2>
+          <p className='text-sm font-normal text-center'>Add items to your catalog to get started</p>
         </div>
         <button 
-          className='w-250 mt-4 flex items-center  border border-gray-500 border-solid border-opacity-30 rounded-lg p-2 bg-secondary text-white'>
+          className='w-fit mt-4 px-8 flex items-center  border border-gray-500 border-solid border-opacity-30 rounded-lg p-2 bg-secondary text-white'>
             Get started
-            <ArrowRightIcon className='w-4 h-4 ml-2 ' />
+            <ArrowRightIcon className='w-4 h-4 ml-4 ' />
         </button>
       </div>
     </Layout>
