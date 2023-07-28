094a017ab10d4be30cd821c0a76d6e314eb2f986
Barbra Mududa <mududabarbs2017@gmail.com>
2023-07-28 17:04:14 +0300

    feat: delete operation

--- src/components/requests/Items.jsx
+++ src/components/requests/Items.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-
+import { MagnifyingGlassIcon, PencilIcon, TrashIcon } from '@heroicons/react/24/outline'
 
 const Items = () => {
   const [formData, setFormData] = useState([]);
@@ -7,14 +7,64 @@ const Items = () => {
   useEffect(() =>{
     fetch("./items")
     .then((res) => res.json())
-    .then((response) => {
-      setFormData()
-    })
+    .then((response) => setFormData(response));
   },[])
-  
+
+  const handleDelete = async (e) => {
+    const id = e.target.id;
+    await fetch("http://localhost:3000/items" + id, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    setFormData(formData.filter((data) => data.id !== id));
+  };
+
+    const items = formData.map((data) => {
+      return (
+        <div key = {data.id} className='w-full flex-row'>
+          <div className='flex-col'>
+            <h3>{data.itemCode}</h3>
+            <p id={data.id}>{data.item}</p>
+          </div>
+          <div className='flex-col'>
+            <h3>Brand</h3>
+            <p>{data.brand}</p>
+          </div>
+          <div className='flex-col'>
+            <h3>UoM</h3>
+            <p>{data.unitOfMeasurement}</p>
+          </div>
+          <div className='flex-col'>
+            <h3>Unit Price</h3>
+            <p>{data.unitPrice}</p>
+          </div>
+          <div className='flex'>
+            <buton className="delete-btn" onClick={handleDelete} id={data.id}>
+              <TrashIcon />
+            </buton>
+            <button>
+              <PencilIcon />
+            </button>
+          </div>
+        </div>
+      )
+    })
+
   return (
-    <div>
-        
+    <div className='container mx-auto mt-8'>
+        <div className='relative'>
+          <input type='text' 
+                className='w-50% px-3 py-2 border rounded-lg focus:border-2 focus:shadow-sm focus:outline-none pl-10'
+                placeholder='Search name, brand'
+          />
+          <MagnifyingGlassIcon className='h-6 w-6 text-grey/500 absolute left-3 top-1/2 transform -translate-y-1/2 pointer-events-none' fill="none" viewBox='0 0 24 24'/>
+        </div>
+        <div>
+          <h3 className='text-base text-sm text-grey/500'>Showing 1 items </h3>
+        </div>
+        <div>{items}</div>
     </div>
   )
 }
--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -1,12 +1,12 @@
-// Catalog.js
 import React from 'react';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Layout from '../components/layout/Layout';
 
+// add { formData} prop
 const Catalog = () => {
   return (
     <Layout>
-      <div className="flex flex-col flex-grow justify-center items-center h-full"> {/* Adjust styles here */}
+      <div className="flex flex-col flex-grow justify-center items-center h-full"> 
         <div className='font-lato'>
           <h2 className='text-base font-semibold text-center'>You have no recorde to show</h2>
           <p className='text-sm font-normal text-center'>Add items to your catalog to get started</p>
@@ -16,6 +16,7 @@ const Catalog = () => {
             Get started
             <ArrowRightIcon className='w-4 h-4 ml-4 ' />
         </button>
+        
       </div>
     </Layout>
   );
