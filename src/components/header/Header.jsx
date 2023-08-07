f058c8885e74c51f5f86a196a502f4702a3f1544
Barbra Mududa <mududabarbs2017@gmail.com>
2023-08-07 16:35:44 +0300

    refactor: header component update

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -46,7 +46,7 @@ const Header = () => {
           {openAddItem && (
             <div className='fixed h-full top-0 right-0 left-0 flex items-start justify-end'>
               <div className='flex flex-row w-2/6  bg-white p-4 rounded-lg'>
-                <AddItem />
+                <AddItem onFormSubmit={() => {}} onClose={handleCloseClick}/>
                 <button className="absolute top-3 right-4 shadow-sm shadow-white items-center justify-center border border-gray-400 rounded-full py-1 px-1"
                   onClick={handleCloseClick}>
                   <XMarkIcon className='w-4 h-4 bg-grey-200' />
